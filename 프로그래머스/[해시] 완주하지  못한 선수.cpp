ace22e7dbbbced6cea5619124e8effe190b03526
Ryu0506 <52912896+Ryu0506@users.noreply.github.com>
2020-09-03 04:40:32 +0900

    Update [해시] 완주하지  못한 선수.cpp

--- 프로그래머스/[해시] 완주하지  못한 선수.cpp
+++ 프로그래머스/[해시] 완주하지  못한 선수.cpp
@@ -1,4 +1,33 @@
-//정렬을 이용한 방법
+/*문제 주소 : https://programmers.co.kr/learn/courses/30/lessons/42576
+개념 정리 : */
+
+// 해시를 이용한 방법
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+string solution(vector<string> participant, vector<string> completion) {
+	string answer = "";
+	unordered_map<string, int> um;
+	for (auto name : participant) {
+		um[name]++;
+	}
+	for (auto name : completion) {
+		um[name]--;
+	}
+	for (auto name : participant) {
+		if (um[name] > 0) {
+			answer = name;
+			break;
+		}
+	}
+	return answer;
+}
+
+// + 정렬을 이용한 방법
 
 #include <string>
 #include <vector>
