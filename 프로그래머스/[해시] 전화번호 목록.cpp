0256b41f125c451c3969170b0e0cc91071d43632
Ryu0506 <52912896+Ryu0506@users.noreply.github.com>
2020-09-03 22:01:40 +0900

    Update [해시] 전화번호 목록.cpp

--- 프로그래머스/[해시] 전화번호 목록.cpp
+++ 프로그래머스/[해시] 전화번호 목록.cpp
@@ -3,7 +3,31 @@
 개념 주소 : 
 */
 
-// 해시를 이용한 방법 (추가 예정)
+// 해시를 이용한 방법
+#include <string>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+bool solution(vector<string> phone_book) {
+	bool answer = true;
+	unordered_map <string, int> hash;
+	for (int i = 0; i < phone_book.size(); i++) {
+		string number = "";
+		for (int j = 0; j < phone_book[i].size(); j++) {
+			number += phone_book[i][j];
+			hash[number] += 1;
+		}
+	}
+	for (auto num : phone_book) {
+		if (hash[num] > 1) {
+			answer = false;
+			break;
+		}
+	}
+	return answer;
+}
 
 // 정렬을 이용한 방법
 #include <string>
